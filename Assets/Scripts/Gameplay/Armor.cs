7616796a43ff10274197515a4419fa359888d049
Erick Subero <dracobk201@gmail.com>
2020-06-10 08:49:14 -0400

    Life System
    - Implemented Life System (UI/Player)

---
+++ Assets/Scripts/Gameplay/Armor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Armor : MonoBehaviour
+{
+    [SerializeField] private IntReference playerActualMaxImpacts;
+    [SerializeField] private IntReference playerRemainingImpacts;
+    [SerializeField] private GameEvent playerDamaged;
+    [SerializeField] private GameEvent playerDefeated;
+
+    private void Start()
+    {
+        Initilize();
+    }
+
+    private void Initilize()
+    {
+        playerRemainingImpacts.Value = playerActualMaxImpacts.Value;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals(Global.EnemyTag))
+            DamageReceived();
+    }
+
+    private void DamageReceived()
+    {
+        playerRemainingImpacts.Value--;
+        playerDamaged.Raise();
+        if (playerRemainingImpacts.Value <= 0)
+        {
+            transform.position = Vector2.zero;
+            playerDefeated.Raise();
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Game/GameplayCanvasActions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayCanvasActions : MonoBehaviour
+{
+    [Header("Player Variables")]
+    [SerializeField] private IntReference maxArmor;
+    [SerializeField] private IntReference actualArmor;
+    [SerializeField] private Image armorGauge;
+
+    public void UpdateArmorGauge()
+    {
+        armorGauge.fillAmount = (float) actualArmor.Value / maxArmor.Value;
+    }
+}
