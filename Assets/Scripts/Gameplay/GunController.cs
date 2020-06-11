27397857bad63f2c2ff6405d98413b6af2907ed4
Erick Subero <dracobk201@gmail.com>
2020-06-11 00:06:44 -0400

    Energy System + Order
    - Implemented energy system.
    - Reordered some assets.

--- Assets/Scripts/Gameplay/GunController.cs
+++ Assets/Scripts/Gameplay/GunController.cs
@@ -1,28 +1,64 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
     [Header("Bullet Variables")]
-    [SerializeField]
-    private BulletsRuntimeSet Bullets;
-    [SerializeField]
-    private GameEvent PlayerShot;
-    [SerializeField]
-    private Transform bulletPosition;
+    [SerializeField] private BulletsRuntimeSet bullets;
+    [SerializeField] private FloatReference actualMaxEnergy;
+    [SerializeField] private FloatReference remainingEnergy;
+    [SerializeField] private FloatReference energyBulletCost;
+    [SerializeField] private FloatReference energyRegenerationAmount;
+    [SerializeField] private FloatReference energyRegenerationTime;
+    [SerializeField] private GameEvent playerShot;
+    [SerializeField] private GameEvent energyRecharged;
+    [SerializeField] private Transform bulletPosition;
+    private bool keepRecharging;
+
+    private void Start()
+    {
+        Initilize();
+    }
+
+    private void Initilize()
+    {
+        remainingEnergy.Value = actualMaxEnergy.Value;
+        keepRecharging = true;
+        StartCoroutine(RechargeEnergy());
+    }
+
+
+    private IEnumerator RechargeEnergy()
+    {
+        var currentTime = energyRegenerationTime.Value;
+        while (keepRecharging)
+        {
+            currentTime -= Time.deltaTime;
+            if (currentTime < 0)
+            {
+                energyRecharged.Raise();
+                remainingEnergy.Value += energyRegenerationAmount.Value;
+                currentTime = energyRegenerationTime.Value;
+            }
+            yield return null;
+        }
+    }
 
     public void ShootBullet()
     {
         var initialPosition = bulletPosition.position;
         var initialRotation = bulletPosition.rotation;
 
-        for (int i = 0; i < Bullets.Items.Count; i++)
+        for (int i = 0; i < bullets.Items.Count; i++)
         {
-            if (!Bullets.Items[i].activeInHierarchy)
+            if (!bullets.Items[i].activeInHierarchy)
             {
-                Bullets.Items[i].transform.localPosition = initialPosition;
-                Bullets.Items[i].transform.localRotation = initialRotation;
-                Bullets.Items[i].SetActive(true);
-                PlayerShot.Raise();
+                bullets.Items[i].transform.localPosition = initialPosition;
+                bullets.Items[i].transform.localRotation = initialRotation;
+                bullets.Items[i].SetActive(true);
+                remainingEnergy.Value -= energyBulletCost.Value;
+                playerShot.Raise();
+                energyRecharged.Raise();
                 break;
             }
         }
--- Assets/Scripts/UI/Game/GameplayCanvasActions.cs
+++ Assets/Scripts/UI/Game/GameplayCanvasActions.cs
@@ -6,7 +6,15 @@ public class GameplayCanvasActions : MonoBehaviour
     [Header("Player Variables")]
     [SerializeField] private IntReference maxArmor;
     [SerializeField] private IntReference actualArmor;
+    [SerializeField] private FloatReference maxEnergy;
+    [SerializeField] private FloatReference actualEnergy;
     [SerializeField] private Image armorGauge;
+    [SerializeField] private Image energyGauge;
+
+    public void UpdateEnergyGauge()
+    {
+        energyGauge.fillAmount = actualEnergy.Value / maxEnergy.Value;
+    }
 
     public void UpdateArmorGauge()
     {
